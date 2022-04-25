072379d9f5bcdb84c5dc5f850aaef665242390df
Tafadzwa Masunda <>
2022-04-25 15:30:48 +0200

    Splash screen and launch icon changes

---
+++ app/src/main/java/com/example/pokemonsplashactivity/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.pokemonsplashactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokemonsplashactivity.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        binding.splashContainer.alpha = 0f
+        binding.splashContainer.animate().setDuration(1500).alpha(1f).withEndAction{
+            val openMainScreenIntent = Intent(this, MainActivity::class.java)
+            startActivity(openMainScreenIntent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+        setContentView(binding.root)
+    }
+}
